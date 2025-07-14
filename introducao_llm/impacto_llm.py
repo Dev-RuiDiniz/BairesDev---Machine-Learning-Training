d6d644c8d7fc4ae7f018fb67528fd75d6da90dc3
Rui Franscisco de Paula <rui.pdiniz@gmail.com>
2025-07-14 14:21:01 -0300

    Create impacto_llm.py

---
+++ introducao_llm/impacto_llm.py
@@ -0,0 +1,11 @@
+# Impactos das LLMs na sociedade:
+# 1. Transformação do mercado de trabalho
+# 2. Acessibilidade à informação
+# 3. Mudanças na educação
+# 4. Desafios éticos e de privacidade
+# 5. Impacto na criatividade e na produção cultural
+# 6. Influência na comunicação e nas redes sociais
+# 7. Potencial para desinformação e manipulação
+# 8. Desenvolvimento de novas tecnologias e inovações
+# 9. Sustentabilidade e eficiência energética
+# 10. Colaboração entre humanos e máquinas
